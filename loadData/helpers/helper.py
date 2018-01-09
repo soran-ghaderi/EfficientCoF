b4a3504c9e145cf0a5ef1aeb58e20d913e97eb53
Soran <soran.gdr.cs@gmail.com>
2018-01-09 03:59:44 +0330

    load_data/helpers/helpers.py: 1. Initiate split function 2. Add docstring

--- loadData/helpers/helper.py
+++ loadData/helpers/helper.py
@@ -2,3 +2,9 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 class Split:
+    def split(self, df):
+        """
+
+        :param df:
+        :return:
+        """
