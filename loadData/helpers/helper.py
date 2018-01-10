0d1ba1796b046773706ef2042a387d7385a6dbaf
Soran <soran.gdr.cs@gmail.com>
2018-01-10 04:08:35 +0330

    load_data/helpers/helpers.py: 1. Modified variable names 3

--- loadData/helpers/helper.py
+++ loadData/helpers/helper.py
@@ -3,27 +3,27 @@ import matplotlib.pyplot as plt
 
 
 class Split:
-    def split(self, df):
+    def split(self, dataframe):
         """
         Split and sort train/test sets
-        :param df: Dataframe containing training data
+        :param dataframe: Dataframe containing training data
         :return: Sorted-test-set/Sorted-train-set dictionaries
         """
-        training_set, test_set = train_test_split(df, test_size=0.2)
+        training_set, test_set = train_test_split(dataframe, test_size=0.2)
         sorted_train_set = training_set.sort_values('user_id')
         sorted_test_set = test_set.sort_values('user_id')
         return sorted_test_set, sorted_train_set
 
 
 class MyPlotter:
-    def plot(self, df):
+    def plot(self, dataframe):
         """
         Plot dataframe distributions:
             1. Distribution of ratings
             2. Distribution of ratings count per user
         :return:
         """
-        df['item_id'].hist(grid=True)
+        dataframe['item_id'].hist(grid=True)
         plt.xlabel('Items')
         plt.ylabel('Frequency')
         plt.title('Distribution of ratings')
