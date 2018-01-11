c53a7e807d8b8d077677139c1e4252659c6b1441
Soran <soran.gdr.cs@gmail.com>
2018-01-11 04:11:47 +0330

    load_data/readf.py: 1. Import necessary libraries

--- loadData/readf.py
+++ loadData/readf.py
@@ -1,18 +1,3 @@
 import pandas as pd
 
 from loadData.helpers import helper as hl
-
-
-class LoadData:
-    def readf(path):
-        """
-        Returns:
-            Train and test set
-        """
-        df = pd.read_csv(path, '\t')
-        df.drop('timestamp', axis=1, inplace=True)
-        print(df['rating'].describe())
-        # hl.MyPlotter.plot(df)
-        sorted_testSet, sorted_trainSet = hl.Split.split(df)
-        return sorted_trainSet, sorted_testSet
-
