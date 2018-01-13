610cdaee1f296bfe0a4377d9c6c09469b415a2c9
Soran <soran.gdr.cs@gmail.com>
2018-01-13 04:17:49 +0330

    load_data/readf.py: 1. Refactor variable names 3

--- loadData/readf.py
+++ loadData/readf.py
@@ -13,5 +13,5 @@ class LoadData:
         dataframe.drop('timestamp', axis=1, inplace=True)
         print(dataframe['rating'].describe())
         # hl.MyPlotter.plot(dataframe)
-        sorted_test_set, sorted_trainSet = hl.Split.split(dataframe)
-        return sorted_trainSet, sorted_test_set
+        sorted_test_set, sorted_train_set = hl.Split.split(dataframe)
+        return sorted_train_set, sorted_test_set
